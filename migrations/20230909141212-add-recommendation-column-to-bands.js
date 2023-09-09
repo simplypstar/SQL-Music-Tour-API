e9654fe6dd50804603d1f27ead460b995c30af8d
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-09-09 10:48:51 -0400

    custom migration using queryInterface

---
+++ migrations/20230909141212-add-recommendation-column-to-bands.js
@@ -0,0 +1,19 @@
+'use strict';
+const { DataTypes } = require('sequelize')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     */
+    
+    await queryInterface.addColumn('bands', 'recommendation', {
+      type: DataTypes.STRING
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('bands', 'recommendation')
+   
+  }
+};
